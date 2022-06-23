37ff1eef1bead794f3326a486c79afd4aa2f9449
Sagar Acharya <sagar.acharya7045@gmail.com>
2022-06-23 10:41:28 +0545

    Ceanup

--- main.go
+++ main.go
@@ -3,14 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// cards := newDeck()
-	// handSize := 3
-	// hand, remainingDeck := deal(cards, handSize)
-	// fmt.Println(cards.toString())
-	// cards.saveToFile("hello.txt")
-	pards, _ := deckFromFile("hello.txt")
-	fmt.Println(pards)
-	//check
-	// hand.print()
-	// remainingDeck.print()
+	cards, _ := deckFromFile("hello.txt")
+	fmt.Println(cards)
 }
