7580712d82fc74bf4fc719fa764acfafdec53e74
Sagar Acharya <sagar.acharya7045@gmail.com>
2022-07-26 09:48:42 +0545

    trying for dynamic checking of the file

--- main.go
+++ main.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 )
 
+// func doesFileExist(filename string) {
+// 	fileName := ""
+// 	_, error := os.Stat(fileName)
+// 	// check if error is "file not exists"
+// 	if os.IsNotExist(error) {
+// 		fmt.Printf("%v file does not exist\n", fileName)
+// 	} else {
+// 		fmt.Printf("%v file exist\n", fileName)
+// 	}
+// }
 func main() {
 	// cards := newDeck()
 	// cards.saveToFile("hello.txt")
+	// doesFileExist("hello.txt")
 	readcards, _ := deckFromFile("hello.txt")
 	fmt.Println(readcards)
 }
